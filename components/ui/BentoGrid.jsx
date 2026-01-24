d0edd1ebe17a1b203a7243f1d219a354a1f52bc1
Vishnu Prakash Suthar <vishnuprakash12.official@gmail.com>
2026-01-24 09:48:51 +0530

    Update BentoGrid.jsx add resume file

--- components/ui/BentoGrid.jsx
+++ components/ui/BentoGrid.jsx
@@ -215,7 +215,7 @@ const BentoGrid = memo(function BentoGrid() {
 
         <Link
           target="_blank"
-          href="/resume.pdf"
+          href="resume_(1).pdf"
           download={true}
           className="flex flex-col mt-auto"
         >
